6300f1f5c8d41534bc74463b77657e4216814201
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2025-10-24 12:08:24 -0300

    mapa feito
    aqui tem só o mapa feito com um marcador, preciso incluir todos os marcadores de todos lugares depois

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-/*let h2 = document.querySelector('h2');
+var map;
+console.log(map);
 
 function success(pos){
     console.log(pos.coords.latitude,pos.coords.longitude);
-    h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`
+
+    if (map === undefined) {
+        mapa = L.map('mapa').setView([pos.coords.latitude, pos.coords.longitude], 15);
+    } else {
+        map.remove();
+        mapa = L.map('mapa').setView([pos.coords.latitude, pos.coords.longitude], 15);
+    }
+
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mapa);
+
+    L.marker([pos.coords.latitude, pos.coords.longitude]) .addTo(mapa)
+        .bindPopup('Você está aqui!')
+        .openPopup();
+
+
+    L.marker([-30.0358182, -51.2143508]) .addTo(mapa)
+            .bindPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>')
+            .openPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>');
 }
+
 
 function error(err){
     console.log(err);
 }
-navigator.geolocation.getCurrentPosition(success);*/
 
-var mapa = L.map('mapa').setView([51.505, -0.09], 13);
+var watchID = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
+//navigator.geolocation.clearWatch(watchID);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mapa);
 
-L.marker([51.5, -0.09]) .addTo(mapa)
-    .bindPopup('fsafsff')
-    .openPopup();
