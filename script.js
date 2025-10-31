3082700ae8b357feda909e84014bb8f7d2eb9eb8
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2025-10-31 11:35:17 -0300

    s

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 //inicio dos codigos do mapa//
-var map;
-console.log(map);
+var mapa;
+console.log(mapa);
 
 function success(pos){
     console.log(pos.coords.latitude,pos.coords.longitude);
 
-    if (map === undefined) {
+    if (mapa === undefined) {
         mapa = L.map('mapa').setView([pos.coords.latitude, pos.coords.longitude], 15);
     } else {
         map.remove();
         mapa = L.map('mapa').setView([pos.coords.latitude, pos.coords.longitude], 15);
     }
 
-
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mapa);
-
-    //adicionar marcadores no mapa:
-    L.marker([pos.coords.latitude, pos.coords.longitude]) .addTo(mapa)
+    
+    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(mapa)
         .bindPopup('Você está aqui!')
         .openPopup();
 
-
-    L.marker([-30.035818, -51.214350]) .addTo(mapa)
+    L.marker([-30.035818, -51.214350]).addTo(mapa)
             .bindPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>')
             .openPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>');
 
-    
-    L.marker([-30.031182, -51.234438]) .addTo(mapa)
+    L.marker([-30.031182, -51.234438]).addTo(mapa)
             .bindPopup('Casa de Cultura Mário Quintana, <a href="marioquintana.html" target="_blank">Clique aqui para ver os shows!</a>')
-            .openPopup('Casa de Cultura Mário Quintana, <a href="marioquintana.html" target="_blank">Clique aqui para ver os shows!</a>');
+            .openPopup('Casa de Cultura Mário Quintana,');
 }
-
 
 function error(err){
     console.log(err);
@@ -45,5 +40,3 @@ var watchID = navigator.geolocation.watchPosition(success, error, {
 });
 //navigator.geolocation.clearWatch(watchID);
 //fim do codigo do mapa//
-
-
