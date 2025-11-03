886360820cecd623fe118b543ad084c88db5e102
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2025-11-03 12:14:36 -0300

    marcadores
    adicionei marcadores aos principais lugares

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //inicio dos codigos do mapa//
 var mapa;
-var marker1, market2, market3;
 console.log(mapa);
 
 
@@ -18,15 +17,20 @@ function success(pos){
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mapa);
 
-    L.marker([51.5, -0.09]).addTo(mapa).bindPopup("I am a green leaf.");
-    L.marker([51.495, -0.083]).addTo(mapa).bindPopup("I am a red leaf.");
-    L.marker([-30.03544, -51.2375]).addTo(mapa).bindPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>');
-    
-    var marker1 = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(mapa)
+    var unisinos = L.marker([-30.02748, -51.17500]).addTo(mapa).bindPopup('Teatro Unisinos, <a href="teatrounisinos.html" target="_blank">Clique aqui para ver os shows!</a>');
+    var margs = L.marker([-30.02907,-51.23169]).addTo(mapa).bindPopup('Museu de Arte do Rio Grande do Sul, <a href="margs.html" target="_blank">Clique aqui para ver as exposições!</a>');
+    var marioquintana = L.marker([-30.03544, -51.2375]).addTo(mapa).bindPopup('Casa de Cultura Mário Quintana, <a href="marioquintana.html" target="_blank">Clique aqui para ver os shows!</a>');
+    var bourboncountry = L.marker([-30.02185, -51.16274]).addTo(mapa).bindPopup('Teatro Bourbon Country, <a href="teatrobourboncountry.html" target="_blank">Clique aqui para ver os shows!</a>');
+    var opiniao = L.marker([-30.04177,-51.22099]).addTo(mapa).bindPopup('Bar Opinião, <a href="opiniao.html" target="_blank">Clique aqui para ver os shows!</a>');
+    var block = L.marker([-30.02368, -51.21237]).addTo(mapa).bindPopup('Block Office, <a href="block.html" target="_blank">Clique aqui para ver os shows!</a>');
+    var urb = L.marker([-29.99699, -51.20062]).addTo(mapa).bindPopup('URB Stage, <a href="urbstage.html" target="_blank">Clique aqui para ver os shows!</a>');
+    var kto = L.marker([-29.98789,-51.17768]).addTo(mapa).bindPopup('KTO Arena, <a href="ktoarena.html" target="_blank">Clique aqui para ver os shows!</a>');
+
+    var localizaçao = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(mapa)
         .bindPopup('Você está aqui!')
         .openPopup();
 
-    var marker2 = L.marker([-30.03580, -51.21445]).addTo(mapa)
+    var araujoviana = L.marker([-30.03580, -51.21445]).addTo(mapa)
             .bindPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>')
             .openPopup('Auditório Araújo Viana, <a href="araujoviana.html" target="_blank">Clique aqui para ver os shows!</a>');
 }
